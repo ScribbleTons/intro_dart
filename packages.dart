c27c0974838358fe4db07b32f16de24d64548eae
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2023-01-14 12:37:43 +0100

    reading on dart and flutter packages

---
+++ packages.dart
@@ -0,0 +1,12 @@
+/**
+ * Understanding how packages are organized in dart
+ * What are their uses and how to use them.
+ */
+
+
+
+
+// Packages contain reusable codes that can be imported and exported
+// to be used in various part of application.
+
+// it modularizes dart component and reusablity 
