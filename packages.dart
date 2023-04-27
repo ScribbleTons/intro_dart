c64e143cd8454ce980980880b4fe96368f37e475
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2023-04-27 20:26:05 +0100

    package

--- packages.dart
+++ packages.dart
@@ -3,10 +3,32 @@
  * What are their uses and how to use them.
  */
 
-
-
-
 // Packages contain reusable codes that can be imported and exported
 // to be used in various part of application.
 
-// it modularizes dart component and reusablity 
+// it modularizes dart component and reusablity
+
+// Playing with classes
+class One {
+  final String a = "";
+
+  set a {
+    this.a = k;
+  }
+
+  String get getA {
+    return this.a;
+  }
+
+  @override
+  String toString() {
+    return this.a;
+  }
+}
+
+void main() {
+  var one = new One();
+  // one.setA = "tell";
+
+  print(one.toString());
+}
